11fe112850c244ead288ef6b65c7697016dfd7e8
aaron.toth <aaron.toth@ontario.ca>
2020-03-01 13:36:42 -0500

    Project path fixes Project now uses relative paths Updated R and Rstudio to the latest Updated project packages (dplyr) Commenting improvements Code improvements More fixes in next commit

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # Protein
-Clustering with Kmeans
 
-### 1. Import data file (.csv) into 1 R object
-### 2. Clustering with 2 attributes and 3 clusters
-### 3. Compare the results
-### 4. Visualize the results
+## Clustering with Kmeans
 
-##### Analysis #####
+1. Import data file (.csv) into 1 R object
+2. Clustering with 2 attributes and 3 clusters
+3. Compare the results
+4. Visualize the results
+
+
+### Analysis
 Part 1 - Plot is very easy to see the 3 clusters.  There is 1 point (sweeden) that could almost go into
 any of the 3 groups.  The cluster furthest to the right (5 points) is unique to the other 2 with respect
-to the RedMeat, where the other 2 clusters more to the left are similar in RedMeat, but unique in WhiteMeat
+to the RedMeat, where the other 2 clusters more to the left are similar in RedMeat, but unique in WhiteMeat.
 The 'withinss' is relatively small as all data points are close the centroid.
+
+#### Notes
+This project makes use of the library(dplyr) for all 'select' functions.  
+To use the script, clone the repo and run the protein1.R script.  Script contains relative paths so this project can be cloned in any location.
--- scripts/protein1.R
+++ scripts/protein1.R
@@ -8,18 +8,19 @@
 # Load dplyr - used for 'select' function
 library(dplyr)
 
-# Set the working directory through the script (instead of session in IDE)
-# This script assumes you have source .csv file(s) in the following folder.  Switch appropriately based on environment
-setwd("E:/school/University.Guelph/8020002 - DEOL Data Mining and Machine Learning/Project Assignment 2/data")
+# Get the cuurent working directory of the running script
+WD = dirname(rstudioapi::getSourceEditorContext()$path)
+if (!is.null(WD)) setwd(WD)
+print(WD)
 
-# Read in rows from the file in the working directory
-protein <- read.csv(file="protein.csv",head=TRUE,sep=",")
+# Read in rows from the file from the 'data' folder in the working directory
+protein <- read.csv(file="../data/protein.csv",head=TRUE,sep=",")
 
 ## PART 1 ##
 # Create copy of protein object (protein1)
 protein1 <- protein
 
-# Remove columns not included in p (part 1 just uses 'RedMeat' and 'WhiteMeat' attributes)
+# Remove columns/attribute not included in p (part 1 just uses 'RedMeat' and 'WhiteMeat' attributes)
 protein1 <- select(protein1, RedMeat, WhiteMeat)
 
 # Clustering with 3 clusters:
@@ -55,7 +56,7 @@ plot(protein2[c("Milk", "Cereals")], col = kmeans.result2$cluster)
 points(kmeans.result2$centers[,c("Milk", "Cereals")], col = 1:3, pch = 4, cex=1)
 
 
-## Analysis - adding as comments in code because assignment asks for one file ##
+## Analysis - inline/code ##
 # Part 1 - Plot is very easy to see the 3 clusters.  There is 1 point (sweeden) that could almost go into
 # any of the 3 groups.  The cluster furthest to the right (5 points) is unique to the other 2 with respect
 # to the RedMeat, where the other 2 clusters more to the left are similar in RedMeat, but unique in WhiteMeat
@@ -63,7 +64,3 @@ points(kmeans.result2$centers[,c("Milk", "Cereals")], col = 1:3, pch = 4, cex=1)
 
 # Part 2 - Plot is a little harder to visualize and see the clusters.  The clusters don't look as defined and the
 # 'withinss' values being higher reflects this.
-
-# The points I took away that resonate with me from this section are 'what are you asking' when doing the clustering
-# exercise on the data points?  I like the example of putting a bunch of pictures of various objects and having
-# the algorithm put them into groups. Groups could end up as "cars", "houses", "People" and "Places".
